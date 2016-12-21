2f021fd1521b8ed4999d53a716235b5be2f3eac4
Gant Laborde <GantMan@gmail.com>
2016-12-21 09:05:46 -0600

    fix wpf package

--- local-cli/rnpm/wpf/index.js
+++ local-cli/rnpm/wpf/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   func: require('./src/wpf'),
   description: 'Generate React Native Windows template project on WPF',
-  name: 'windows [name]',
+  name: 'wpf [name]',
   options: [{
     command: '--windowsVersion [version]',
     description: 'The version of react-native-wpf to use.'
